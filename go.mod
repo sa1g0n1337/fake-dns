a02aa935ed97661916c2324726007c1a8c1523fc
vinh <vinh@localhost.localdomain>
2021-03-25 22:43:50 +0700

    update

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module fake-dns
 
 go 1.16
 
-require github.com/miekg/dns v1.1.40
+require (
+	github.com/miekg/dns v1.1.40
+	github.com/tidwall/gjson v1.7.1
+)
--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"flag"
 	"log"
+	"os"
+	"sort"
 	"strings"
 	"time"
 
 	"github.com/miekg/dns"
+	"github.com/tidwall/gjson"
 )
 
 const internalIPv4 = "192.168.1.143"
@@ -24,12 +27,43 @@ func isDomain(child, parrent string) bool {
 
 func getIP(q dns.Question) string {
 	domain := strings.Trim(q.Name, ".")
-	if isDomain(domain, "hrservices.vn") {
-		if q.Qtype == dns.TypeA {
-			return internalIPv4
-		}
-		if q.Qtype == dns.TypeAAAA {
-			return "" // reject record
+	for ip, domains := range domainIPMap {
+		for _, dm := range domains {
+			neg := false
+			if dm[0] == '!' {
+				neg = true
+				dm = dm[1:]
+			}
+			if dm[0] == '.' {
+				if !isDomain(domain, dm[1:]) {
+					continue
+				}
+			} else if domain != dm {
+				continue
+			}
+			if neg {
+				return "-"
+			}
+			// matched and not neg
+			if ip[0] == 'A' {
+				if ip[1] == 'A' {
+					// IPv6
+					if q.Qtype == dns.TypeAAAA {
+						return ip[4:]
+					}
+					return ""
+				}
+				// IPv4
+				if q.Qtype == dns.TypeA {
+					return ip[1:]
+				}
+				return ""
+			}
+			// IPv4
+			if q.Qtype == dns.TypeA {
+				return ip
+			}
+			return ""
 		}
 	}
 
@@ -66,8 +100,9 @@ func responseQuery(reqMsg, replyMsg *dns.Msg) {
 			if len(rr) == 0 {
 				log.Println("Query", dns.Type(q.Qtype).String(), ":", q.Name, "- fallback nil")
 			} else {
-				replyMsg.Answer = append(replyMsg.Answer, rr[0])
-				log.Println("Query", dns.Type(q.Qtype).String(), ":", q.Name, "==>", rr[0].String()[len(rr[0].Header().String()):])
+				replyMsg.Answer = append(replyMsg.Answer, rr...)
+				rri := rr[len(rr)-1]
+				log.Println("Query", dns.Type(q.Qtype).String(), ":", q.Name, "==", dns.Type(rri.Header().Rrtype).String(), "=>", rri.String()[len(rri.Header().String()):])
 			}
 			continue
 		}
@@ -101,9 +136,46 @@ func handler(writer dns.ResponseWriter, reqMsg *dns.Msg) {
 }
 
 var bindAddr = flag.String("addr", ":53", "UDP bind address")
+var configFile = flag.String("config", "config.json", "Config JSON file")
+var domainIPMap = map[string][]string{}
 
 func main() {
 	flag.Parse()
+
+	content, err := os.ReadFile(*configFile)
+	if err != nil {
+		log.Panicln(err)
+	}
+	d := gjson.ParseBytes(content)
+	if !d.IsObject() {
+		log.Panicln("File errorr:", *configFile)
+	}
+	for key, val := range d.Map() {
+		if !val.IsArray() {
+			continue
+		}
+		arr, _ := domainIPMap[key]
+		for _, d := range val.Array() {
+			domain := d.String()
+			if len(domain) < 2 {
+				continue
+			}
+			arr = append(arr, domain)
+		}
+		sort.Slice(arr, func(i, j int) bool {
+			if arr[i][0] == '!' {
+				if arr[j][0] == '!' {
+					return false
+				}
+				return true
+			}
+			return false
+		})
+		domainIPMap[key] = arr
+	}
+
+	log.Println(domainIPMap)
+
 	dns.HandleFunc(".", handler)
 	server := &dns.Server{
 		Addr: *bindAddr,
@@ -111,7 +183,7 @@ func main() {
 	}
 	log.Println("Started at:", *bindAddr)
 
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	if err != nil {
 		log.Println("Failed to start server:", err.Error())
 	}
