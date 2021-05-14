1504d2ea4da1700ec427c1f1452a8a96bbc44dee
vinhjaxt <ngoc.lo.dang@gmail.com>
2021-05-14 20:43:31 +0700

    Update ios-disable-ssl-pinning.sh

--- ios-disable-ssl-pinning.sh
+++ ios-disable-ssl-pinning.sh
@@ -1,2 +1,17 @@
 #!/usr/bin/env bash
-frida -Uf "$1" -l ios-flutter-disable-sslpinning.js --no-pause
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
+pid="$(frida-ps -Ua | grep "$1" | cut -d ' ' -f 1)"
+
+if [ "$pid" == "" ]; then
+  echo 'Please open the app first.'
+  exit 1
+fi
+
+frida -U "$pid" -l "$DIR/ios-flutter-disable-sslpinning.js" --no-pause
